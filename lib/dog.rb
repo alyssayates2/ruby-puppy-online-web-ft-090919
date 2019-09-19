382adf12e715cb6229b786bac7bd73c936855a70
Alyssa Yates <bosslys@icloud.com>
2019-09-19 19:41:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,31 +1,41 @@
 class Dog
 
+  attr_accessor :name
 
   @@all = []
 
   def initialize(name)
     @name = name
     @@all << self
-
-
-  def self.all=(name)
-    @self.all = @@all
   end
 
   def self.all
-    @self.all
+    @@all
+  end
+
+#   class Album
+#  
+#   def self.all=(name)
+#     @self.all = @@all
+#   end
+#  
+#   def self.all
+#     @self
+#   end
+# end
+# 
+#   Write a class method, `.all`, that reads this variable. From inside the `Dog`
+#    class, we can access the `@@all` class variable, but whenever we might be
+#    interacting with our `Dog` class from the outside, this `.all` class method acts
+#    as our direct interface to the `@@all` variable.
+
+   
+  def Dog.clear_all
+      self.all.clear
   end
 
     def self.print_all
       self.all.each{|dog| puts "#{dog.name}"}
     end
-
-  end
-
-  def Dog.clear_all
-      self.all.clear
-  end
-
-
-
+    
 end

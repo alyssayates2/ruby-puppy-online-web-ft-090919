7fd6ccf29de0f92df9c7639385052b72710a6253
Alyssa Yates <bosslys@icloud.com>
2019-09-19 19:15:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,8 +5,10 @@ class Dog
   @@all = []
 
   def initialize(name)
+    def save
     @name = name
     @@all << self
+  end 
   end
 
   def self.all
@@ -20,5 +22,5 @@ class Dog
     def self.print_all
       self.all.each{|dog| puts "#{dog.name}"}
     end
-    
+
 end

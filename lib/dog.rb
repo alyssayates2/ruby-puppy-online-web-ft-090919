82e79252104dc30327596ce6264366640c792bd3
Alyssa Yates <bosslys@icloud.com>
2019-09-19 19:41:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -14,22 +14,22 @@ class Dog
   end
 
 #   class Album
-#  
+#
 #   def self.all=(name)
 #     @self.all = @@all
 #   end
-#  
+#
 #   def self.all
 #     @self
 #   end
 # end
-# 
+#
 #   Write a class method, `.all`, that reads this variable. From inside the `Dog`
 #    class, we can access the `@@all` class variable, but whenever we might be
 #    interacting with our `Dog` class from the outside, this `.all` class method acts
 #    as our direct interface to the `@@all` variable.
 
-   
+
   def Dog.clear_all
       self.all.clear
   end
@@ -37,5 +37,5 @@ class Dog
     def self.print_all
       self.all.each{|dog| puts "#{dog.name}"}
     end
-    
+
 end

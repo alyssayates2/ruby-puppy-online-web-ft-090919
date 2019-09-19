cfcdb000f7abafea9a552de40b48bb0d01828c53
Alyssa Yates <bosslys@icloud.com>
2019-09-19 18:48:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -13,14 +13,14 @@ class Dog
 
   def Dog.clear_all
       self.all.clear
-    end
-
-    def self.print_all
-    self.all.each{|dog| puts "#{dog.name}"}
   end
 
-def save
-  self.save << @@all 
-end
+    def self.print_all
+      self.all.each{|dog| puts "#{dog.name}"}
+    end
+
+    def save
+      self.save << @@all
+    end
 
 end
